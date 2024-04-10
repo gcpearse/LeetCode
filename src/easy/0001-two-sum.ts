763bf5f0dffc45d0d9e19fd49263318ebb472794
gcpearse <glen.c.pearse@gmail.com>
2024-04-11 00:21:39 +0100

    reformatting

--- src/easy/0001-two-sum.ts
+++ src/easy/0001-two-sum.ts
@@ -3,7 +3,9 @@ function twoSum(nums: number[], target: number): number[] {
   let res: number[] = []
 
   for (let i = 0; i < nums.length; i++) {
+
     for (let j = 0; j < nums.length; j++) {
+      
       if (nums[i] + nums[j] == target && i !== j) {
         res = [i, j]
       }
