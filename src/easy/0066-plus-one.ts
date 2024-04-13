62b15b0fde485db8bf293c3ca815b703060453b9
gcpearse <glen.c.pearse@gmail.com>
2024-04-13 22:30:56 +0100

    removes console logs

--- src/easy/0066-plus-one.ts
+++ src/easy/0066-plus-one.ts
@@ -33,28 +33,6 @@ function plusOne(digits: number[]): number[] {
 }
 
 
-console.log(plusOne([1, 2, 3]))
-// [1, 2, 4]
-console.log(plusOne([4, 3, 2, 1]))
-// [4, 3, 2, 2]
-console.log(plusOne([9]))
-// [1, 0]
-console.log(plusOne([9, 9]))
-// [1, 0, 0]
-console.log(plusOne([9, 9, 9]))
-// [1, 0, 0, 0]
-console.log(plusOne([8, 9, 9, 9]))
-// [9, 0, 0, 0]
-console.log(plusOne([8, 0, 9, 9]))
-// [8, 1, 0, 0]
-console.log(plusOne([9, 8, 9]))
-// [9, 9, 0]
-console.log(plusOne([2, 4, 9, 3, 9]))
-// [2, 4, 9, 4, 0]
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]))
-// [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]
-
-
 /*
 
 You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
