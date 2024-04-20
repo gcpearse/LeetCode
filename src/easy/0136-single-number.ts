4803677eb67cd944aafaac297c37c82b91714e20
gcpearse <glen.c.pearse@gmail.com>
2024-04-20 21:58:19 +0100

    refactors to use ternary and removes comment

--- src/easy/0136-single-number.ts
+++ src/easy/0136-single-number.ts
@@ -3,11 +3,7 @@ function singleNumber(nums: number[]): number {
   const set = new Set<number>()
 
   for (const num of nums) {
-    if (!set.has(num)) {
-      set.add(num)
-    } else {
-      set.delete(num)
-    }
+    !set.has(num) ? set.add(num) : set.delete(num)
   }
 
   return [...set][0]
@@ -20,7 +16,6 @@ Given a non-empty array of integers nums, every element appears twice except for
 
 You must implement a solution with a linear runtime complexity and use only constant extra space.
 
- 
 Example 1:
 
 Input: nums = [2, 2, 1]
@@ -37,30 +32,3 @@ Input: nums = [1]
 Output: 1
 
 */
-
-// function singleNumber(nums: number[]): number {
-
-//   let res: number = 0
-
-//   type Tally = {
-//     [key: number]: number
-//   }
-
-//   const tally: Tally = {}
-
-//   for (const num of nums) {
-//     if (!tally[num]) {
-//       tally[num] = 1
-//     } else {
-//       tally[num]++
-//     }
-//   }
-
-//   for (const key in tally) {
-//     if (tally[key] === 1) {
-//       res = +key
-//     }
-//   }
-
-//   return res
-// }
