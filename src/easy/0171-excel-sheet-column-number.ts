a0addd0fd921bc759ee0ae071a2c330ad82efe48
gcpearse <glen.c.pearse@gmail.com>
2024-04-23 00:02:08 +0100

    adds solution to problem

---
+++ src/easy/0171-excel-sheet-column-number.ts
@@ -0,0 +1,47 @@
+function titleToNumber(columnTitle: string): number {
+
+  const letters = columnTitle.split("")
+  let multiplier = 1
+  let result = 0
+
+  for (let i = letters.length - 1; i >= 0; i--) {
+    result += (columnTitle.charCodeAt(i) - 64) * multiplier
+    multiplier *= 26
+  }
+
+  return result
+}
+
+
+/*
+
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnTitle = "A"
+Output: 1
+
+Example 2:
+
+Input: columnTitle = "AB"
+Output: 28
+
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701
+
+*/
