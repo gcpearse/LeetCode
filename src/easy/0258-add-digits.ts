11c5975539698cd66e6eb6bcdc2be01c00ffd8d0
gcpearse <glen.c.pearse@gmail.com>
2024-04-25 12:19:09 +0100

    adds recursive solution to problem

---
+++ src/easy/0258-add-digits.ts
@@ -0,0 +1,27 @@
+function addDigits(num: number): number {
+
+  if (String(num).length === 1) return num
+
+  return addDigits(String(num).split("").map(digit => +digit).reduce((a, b) => a + b))
+}
+
+
+/*
+
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+
+Example 2:
+
+Input: num = 0
+Output: 0
+
+*/
