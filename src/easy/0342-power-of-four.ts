86b9460ec3c66c46644a1916cecfd065ca1f7acd
gcpearse <glen.c.pearse@gmail.com>
2024-04-27 00:07:46 +0100

    adds solution to problem

---
+++ src/easy/0342-power-of-four.ts
@@ -0,0 +1,41 @@
+function isPowerOfFour(n: number): boolean {
+
+  if (n < 0) return false
+
+  let multiplier = 0
+
+  while (4 ** multiplier <= n) {
+    if (4 ** multiplier === n) {
+      return true
+    } else {
+      multiplier++
+    }
+  }
+
+  return false
+}
+
+
+/*
+
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4^x.
+
+
+Example 1:
+
+Input: n = 16
+Output: true
+
+Example 2:
+
+Input: n = 5
+Output: false
+
+Example 3:
+
+Input: n = 1
+Output: true
+
+*/
