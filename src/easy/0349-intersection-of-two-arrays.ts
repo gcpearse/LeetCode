208ee7c7ffe988b4d7814af25940cc751ffef26c
gcpearse <glen.c.pearse@gmail.com>
2024-04-28 17:09:53 +0100

    adds solution to problem

---
+++ src/easy/0349-intersection-of-two-arrays.ts
@@ -0,0 +1,31 @@
+function intersection(nums1: number[], nums2: number[]): number[] {
+
+  const set = new Set<number>()
+
+  for (const num of nums1) {
+    if (nums2.includes(num)) {
+      set.add(num)
+    }
+  }
+
+  return [...set]
+}
+
+
+/*
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. 
+Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+
+Input: nums1 = [1, 2, 2, 1], nums2 = [2, 2]
+Output: [2]
+
+Example 2:
+
+Input: nums1 = [4, 9, 5], nums2 = [9, 4, 9, 8, 4]
+Output: [9, 4]
+Explanation: [4, 9] is also accepted.
+
+*/
