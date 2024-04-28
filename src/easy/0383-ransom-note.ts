cb7d55b101856268ea8d7411e77cba56cb7e82b3
gcpearse <glen.c.pearse@gmail.com>
2024-04-29 00:02:55 +0100

    adds solution to problem

---
+++ src/easy/0383-ransom-note.ts
@@ -0,0 +1,66 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+
+  const magazineArray = magazine.split("")
+
+  for (const char of ransomNote) {
+    if (!magazineArray.includes(char)) return false
+    magazineArray.splice(magazineArray.indexOf(char), 1)
+  }
+
+  return true
+}
+
+
+/*
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+*/
+
+
+  // type Tally = {
+  //   [key: string]: number
+  // }
+
+  // const ransomNoteTally: Tally = {}
+  // const magazineTally: Tally = {}
+
+  // for (const char of ransomNote) {
+  //   if (!ransomNoteTally[char]) {
+  //     ransomNoteTally[char] = 1
+  //   } else {
+  //     ransomNoteTally[char]++
+  //   }
+  // }
+
+  // for (const char of magazine) {
+  //   if (!magazineTally[char]) {
+  //     magazineTally[char] = 1
+  //   } else {
+  //     magazineTally[char]++
+  //   }
+  // }
+
+  // for (const key in ransomNoteTally) {
+  //   if (!magazineTally[key]) return false
+  //   if (ransomNoteTally[key] > magazineTally[key]) return false
+  // }
+
+  // return true
