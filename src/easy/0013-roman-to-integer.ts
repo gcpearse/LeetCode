08ba5f5f0f7f55b34cceef6eb149929e86f5fcfb
gcpearse <glen.c.pearse@gmail.com>
2024-05-17 18:48:33 +0100

    exports function

--- src/easy/0013-roman-to-integer.ts
+++ src/easy/0013-roman-to-integer.ts
@@ -1,4 +1,4 @@
-function romanToInt(s: string): number {
+export function romanToInt(s: string): number {
 
   type Map = {
     [key: string]: number
@@ -54,6 +54,25 @@ There are six instances where subtraction is used:
 I can be placed before V (5) and X (10) to make 4 and 9. 
 X can be placed before L (50) and C (100) to make 40 and 90. 
 C can be placed before D (500) and M (1000) to make 400 and 900.
+
 Given a roman numeral, convert it to an integer.
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 */
