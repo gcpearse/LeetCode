0f02cb17b73157b418a91f8cb7c34c7729e73d0f
gcpearse <glen.c.pearse@gmail.com>
2024-06-05 15:17:29 +0100

    updates test expectations for twoSum

--- src/__tests__/easy.test.ts
+++ src/__tests__/easy.test.ts
@@ -49,9 +49,9 @@ import { distributeCandies } from "../easy/0575-distribute-candies"
 
 describe("twoSum", () => {
   test("Returns the correct indices", () => {
-    expect(twoSum([2, 7, 11, 15], 9)).toEqual([1, 0])
-    expect(twoSum([3, 2, 4], 6)).toEqual([2, 1])
-    expect(twoSum([3, 3], 6)).toEqual([1, 0])
+    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
+    expect(twoSum([3, 2, 4], 6)).toEqual([1, 2])
+    expect(twoSum([3, 3], 6)).toEqual([0, 1])
   })
 })
 
