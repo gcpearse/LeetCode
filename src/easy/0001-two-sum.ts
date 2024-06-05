8b01f8bff498fee38f0a76f73ecb44d36adaa53c
gcpearse <glen.c.pearse@gmail.com>
2024-06-05 15:17:07 +0100

    reverses order of results

--- src/easy/0001-two-sum.ts
+++ src/easy/0001-two-sum.ts
@@ -7,7 +7,7 @@ export function twoSum(nums: number[], target: number): number[] {
     for (let j = 0; j < nums.length; j++) {
       
       if (nums[i] + nums[j] == target && i !== j) {
-        res = [i, j]
+        res = [j, i]
       }
     }
   }
