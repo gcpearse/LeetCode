44c6efe3fa645d4aa4edfe509db868bb262e22b3
gcpearse <glen.c.pearse@gmail.com>
2024-06-06 22:45:04 +0100

    adds further test cases for majorityElement

--- src/__tests__/easy.test.ts
+++ src/__tests__/easy.test.ts
@@ -190,6 +190,8 @@ describe("majorityElement", () => {
   test("Returns the majority element in the input array", () => {
     expect(majorityElement([3, 2, 3])).toBe(3)
     expect(majorityElement([2, 2, 1, 1, 1, 2, 2])).toBe(2)
+    expect(majorityElement([1])).toBe(1)
+    expect(majorityElement([3, 3, 4])).toBe(3)
   })
 })
 
