983ba96e5f56d93e8d9c8d827325a1032ec691f0
gcpearse <glen.c.pearse@gmail.com>
2024-06-08 15:49:29 +0100

    fixes line spacing

--- src/easy/0268-missing-number.ts
+++ src/easy/0268-missing-number.ts
@@ -17,7 +17,6 @@ export function missingNumber(nums: number[]): number {
 
 Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
-
 Example 1:
 
 Input: nums = [3, 0, 1]
