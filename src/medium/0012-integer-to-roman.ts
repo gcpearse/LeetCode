a1ee14ff01d44a2f301695fffaad5d8d8748ae58
gcpearse <glen.c.pearse@gmail.com>
2024-06-24 18:05:30 +0100

    refactoring

--- src/medium/0012-integer-to-roman.ts
+++ src/medium/0012-integer-to-roman.ts
@@ -1,47 +1,30 @@
 export function intToRoman(num: number): string {
 
+  const pairs = [
+    { roman: "M", int: 1000 },
+    { roman: "CM", int: 900 },
+    { roman: "D", int: 500 },
+    { roman: "CD", int: 400 },
+    { roman: "C", int: 100 },
+    { roman: "XC", int: 90 },
+    { roman: "L", int: 50 },
+    { roman: "XL", int: 40 },
+    { roman: "X", int: 10 },
+    { roman: "IX", int: 9 },
+    { roman: "V", int: 5 },
+    { roman: "IV", int: 4 },
+    { roman: "I", int: 1 }
+  ]
+
   let result = ""
+  let index = 0
 
   while (num > 0) {
-    if (num - 1000 >= 0) {
-      result += "M"
-      num -= 1000
-    } else if (num - 900 >= 0) {
-      result += "CM"
-      num -= 900
-    } else if (num - 500 >= 0) {
-      result += "D"
-      num -= 500
-    } else if (num - 400 >= 0) {
-      result += "CD"
-      num -= 400
-    } else if (num - 100 >= 0) {
-      result += "C"
-      num -= 100
-    } else if (num - 90 >= 0) {
-      result += "XC"
-      num -= 90
-    } else if (num - 50 >= 0) {
-      result += "L"
-      num -= 50
-    } else if (num - 40 >= 0) {
-      result += "XL"
-      num -= 40      
-    } else if (num - 10 >= 0) {
-      result += "X"
-      num -= 10      
-    } else if (num - 9 >= 0) {
-      result += "IX"
-      num -= 9      
-    } else if (num - 5 >= 0) {
-      result += "V"
-      num -= 5     
-    } else if (num - 4 >= 0) {
-      result += "IV"
-      num -= 4      
-    } else if (num - 1 >= 0) {
-      result += "I"
-      num -= 1     
+    if (num - pairs[index].int >= 0) {
+      result += pairs[index].roman
+      num -= pairs[index].int
+    } else {
+      index ++
     }
   }
 
