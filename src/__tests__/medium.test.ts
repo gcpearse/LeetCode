285c13eb111d9802db5f61e25ac1e92106c8c8d8
gcpearse <glen.c.pearse@gmail.com>
2024-06-24 23:46:40 +0100

    adds tests for lengthOfLongestSubstring

--- src/__tests__/medium.test.ts
+++ src/__tests__/medium.test.ts
@@ -1,3 +1,4 @@
+import { lengthOfLongestSubstring } from "../medium/0003-longest-substring-without-repeating-characters"
 import { intToRoman } from "../medium/0012-integer-to-roman"
 
 
@@ -6,5 +7,14 @@ describe("intToRoman", () => {
     expect(intToRoman(3749)).toBe("MMMDCCXLIX")
     expect(intToRoman(58)).toBe("LVIII")
     expect(intToRoman(3749)).toBe("MMMDCCXLIX")
+  })
+})
+
+
+describe("lengthOfLongestSubstring", () => {
+  test("Returns the length of the longest substring without repeating characters", () => {
+    expect(lengthOfLongestSubstring("abcabcbb")).toBe(3)
+    expect(lengthOfLongestSubstring("bbbbb")).toBe(1)
+    expect(lengthOfLongestSubstring("pwwkew")).toBe(3)
   })
 })
