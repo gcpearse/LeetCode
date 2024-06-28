85d6e00ace05b7a4768f9b7bfe36be65ae064dac
gcpearse <glen.c.pearse@gmail.com>
2024-06-28 19:05:33 +0100

    adds tests for fullJustify

--- src/__tests__/hard.test.ts
+++ src/__tests__/hard.test.ts
@@ -1,5 +1,6 @@
 import { trap } from "../hard/0042-trapping-rain-water"
 import { isNumber } from "../hard/0065-valid-number"
+import { fullJustify } from "../hard/0068-text-justification"
 
 
 describe("trap", () => {
@@ -19,5 +20,29 @@ describe("isNumber", () => {
     expect(isNumber("e")).toBe(false)
     expect(isNumber(".")).toBe(false)
     expect(isNumber(".1.")).toBe(false)
+  })
+})
+
+
+describe("fullJustify", () => {
+  test("Returns an array of fully justified text", () => {
+    expect(fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16)).toEqual([
+      "This    is    an",
+      "example  of text",
+      "justification.  "
+   ])
+    expect(fullJustify(["What", "must", "be", "acknowledgment", "shall", "be"], 16)).toEqual([
+      "What   must   be",
+      "acknowledgment  ",
+      "shall be        "
+    ])
+    expect(fullJustify(["Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art","is", "everything", "else", "we", "do"], 20)).toEqual([
+      "Science  is  what we",
+      "understand      well",
+      "enough to explain to",
+      "a  computer.  Art is",
+      "everything  else  we",
+      "do                  "
+    ])
   })
 })
