ca65bc07c0e33439f320da8f0b331d1c73cc52fb
gcpearse <glen.c.pearse@gmail.com>
2024-11-04 12:41:41 +0000

    Refactoring

--- src/easy/0035-search-input-position.ts
+++ src/easy/0035-search-input-position.ts
@@ -1,22 +1,10 @@
 export function searchInsert(nums: number[], target: number): number {
 
-  let res = 0
-
-  if (nums.includes(target)) {
-    return nums.indexOf(target)
-  } else if (target <= nums[0]) {
-    return 0
-  } else if (target > nums[nums.length -1]) {
-    return nums.length
-  } else {
-    for (let i = 0; i < nums.length - 1; i++) {
-      if (target > nums[i] && target <= nums[i + 1]) {
-        res = i + 1
-      }
-    }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= target) return i
   }
 
-  return res
+  return nums.length
 }
 
 
